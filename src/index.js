e1309083c4a2c7b4f3bc49952a379e28e70a085d
Davy Peter BRAUN <hello@davybraun.com>
2022-06-23 09:41:08 +0200

    feat: persist the store using redux-persist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import './index.css';
 import App from './App';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate
+      loading={(<div>Loading...</div>)}
+      persistor={persistor}
+    >
+      <App />
+    </PersistGate>
   </Provider>
 );
